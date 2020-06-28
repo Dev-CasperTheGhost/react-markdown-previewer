19c0339023f0c630e90cd7653237ba7d5c3f7333
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-06-28 18:46:51 +0200

    :tada: Inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Markdown from "react-markdown";
 
 function App() {
+  const [mdInput, setMdInput] = useState("# hello world");
+
+  const copyText = () => {
+    const textarea = document.querySelector(".input-container textarea");
+    textarea.select();
+    textarea.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h3>Markdown previewer</h3>
+        <div>
+          <a className="header-link" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">Cheat sheet</a>
+          <a className="header-link" href="https://npmjs.org/package/react-markdown">react-markdown</a>
+        </div>
       </header>
+      <div className="container">
+        <div className="previewer">
+          <div className="input-container">
+            <div onClick={copyText} className="copy">Copy</div>
+            <textarea value={mdInput} onChange={e => { setMdInput(e.target.value) }}>
+            </textarea>
+          </div>
+          <div className="preview">
+            <Markdown source={mdInput} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
