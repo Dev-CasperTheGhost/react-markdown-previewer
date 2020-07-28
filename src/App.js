448201f42b25cec6eba0b21192bdefa6c2adfa53
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-07-28 16:21:43 +0200

    :package: Updated packages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Markdown from "react-markdown";
 
 function App() {
@@ -7,25 +7,43 @@ function App() {
   const copyText = () => {
     const textarea = document.querySelector(".input-container textarea");
     textarea.select();
-    textarea.setSelectionRange(0, 99999)
+    textarea.setSelectionRange(0, 99999);
     document.execCommand("copy");
-  }
+  };
 
   return (
     <div className="App">
       <header className="header">
-        <h3>Markdown previewer</h3>
+        <h3>
+          <a className="header-link" href="/">Markdown previewer</a>
+        </h3>
         <div>
-          <a className="header-link" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">Cheat sheet</a>
-          <a className="header-link" href="https://npmjs.org/package/react-markdown">react-markdown</a>
+          <a
+            className="header-link"
+            href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet"
+          >
+            Cheat sheet
+          </a>
+          <a
+            className="header-link"
+            href="https://npmjs.org/package/react-markdown"
+          >
+            react-markdown
+          </a>
         </div>
       </header>
       <div className="container">
         <div className="previewer">
           <div className="input-container">
-            <div onClick={copyText} className="copy">Copy</div>
-            <textarea value={mdInput} onChange={e => { setMdInput(e.target.value) }}>
-            </textarea>
+            <div onClick={copyText} className="copy">
+              Copy
+            </div>
+            <textarea
+              value={mdInput}
+              onChange={(e) => {
+                setMdInput(e.target.value);
+              }}
+            ></textarea>
           </div>
           <div className="preview">
             <Markdown source={mdInput} />
